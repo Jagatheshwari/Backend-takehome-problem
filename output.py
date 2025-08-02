68ce9d6c494e8c5f331a2263cb819c4d64e9e6c5
Jagatheshwari <80768066+Jagatheshwari@users.noreply.github.com>
2025-08-02 19:25:03 +0530

    Create output.py

---
+++ output.py
@@ -0,0 +1,16 @@
+[tool.poetry]
+name = "get-papers"
+version = "0.1.0"
+description = "Fetch PubMed papers with pharma affiliations"
+authors = ["Your Name <you@example.com>"]
+
+[tool.poetry.dependencies]
+python = "^3.8"
+biopython = "^1.81"
+
+[tool.poetry.scripts]
+get-papers-list = "cli:main"
+
+[build-system]
+requires = ["poetry-core"]
+build-backend = "poetry.core.masonry.api"
